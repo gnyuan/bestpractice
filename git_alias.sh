a5faa35bfe718f2e35e8b888e4dc617d6992c7d3
gnyuan <gnyuan@gmail.com>
2019-08-27 22:40:26 +0800

    add .vimrc .bash_profile git_alias for my own

---
+++ git_alias.sh
@@ -0,0 +1,10 @@
+git config --global user.name="gnyuan"
+git config --global user.email="xx@xx.com"
+git config --global alias.st status
+git config --global alias.co checkout
+git config --global alias.ci commit
+git config --global alias.br branch
+git config --global alias.unstage "reset HEAD"
+git config --global alias.last "log -1"
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
