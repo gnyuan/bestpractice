4fc08b42b407de81aab587ebf1b024f115119354
yuangn <yuangn@ghzq.com.cn>
2021-07-29 17:37:29 +0800

    fix

--- git_alias.sh
+++ git_alias.sh
@@ -1,5 +1,5 @@
 git config --global user.name "gnyuan"
-git config --global user.email "xx@xx.com"
+git config --global user.email "xx@xx.com" 
 git config --global alias.st status
 git config --global alias.co checkout
 git config --global alias.ci commit
@@ -10,3 +10,4 @@ git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Crese
 git config --global core.autocrlf true
 git config --global http.proxy 'socks5://127.0.0.1:10808'
 git config --global https.proxy 'socks5://127.0.0.1:10808'
+
