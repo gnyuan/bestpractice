b49693d1390a5ba2ea2942956640a714a9dac717
yuangn <yuangn@ghzq.com.cn>
2021-07-29 17:34:40 +0800

    add proxy

--- git_alias.sh
+++ git_alias.sh
@@ -8,4 +8,5 @@ git config --global alias.unstage "reset HEAD"
 git config --global alias.last "log -1"
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 git config --global core.autocrlf true
-
+git config --global http.proxy 'socks5://127.0.0.1:10808'
+git config --global https.proxy 'socks5://127.0.0.1:10808'
