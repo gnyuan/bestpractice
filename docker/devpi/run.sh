c6e0564eb6fd2d24b7ea3ffc8dbd0164760c1f35
yuangn <yuangn@ccfund.com.cn>
2023-06-27 19:18:22 +0800

    update devpi

--- docker/devpi/run.sh
+++ docker/devpi/run.sh
@@ -1,7 +1,8 @@
+# 首次需要初始化，后面就不需要了
+# devpi-init && \
 docker run -d --name devpi -p 3141:3141 \
 --restart always -v ./root:/root:rw python:3.10-slim \
 /bin/bash -c "pip install -i https://mirrors.aliyun.com/pypi/simple/ --upgrade pip && \
 pip install -q -U devpi-server==6.9.0 supervisor -i https://mirrors.aliyun.com/pypi/simple/  && \
-devpi-init && \
 devpi-server --host 0.0.0.0 --port 3141"
 
