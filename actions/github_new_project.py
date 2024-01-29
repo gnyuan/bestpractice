c5687ac1f9f3fffa769c07fc67a75a2242628341
yuangn <yuangn@yuangn.com>
2024-01-29 09:36:00 +0800

    feat: add free quota monitor

--- actions/github_new_project.py
+++ actions/github_new_project.py
@@ -1,22 +1,52 @@
 import requests
 import json
-from datetime import datetime, timedelta
+import datetime as dt
+import os
 
-# Calculate the date one month ago
-one_month_ago = (datetime.now() - timedelta(days=30)).strftime('%Y-%m-%d')
 
-params = {
-    'sort': 'stars',
-    'order': 'desc',
-    'q': f'language:python created:>{one_month_ago} stars:>5001',
-}
+def get_github_repo_html():
+    # Calculate the date one month ago
+    one_month_ago = (dt.datetime.now() - dt.timedelta(days=30)
+                     ).strftime('%Y-%m-%d')
 
-response = requests.get('https://api.github.com/search/repositories', params=params)
-content = json.loads(response.content)
+    params = {
+        'sort': 'stars',
+        'order': 'desc',
+        'q': f'language:python created:>{one_month_ago} stars:>5001',
+    }
 
-html_content = "<html><body><table><tr><th>Name</th><th>HTML URL</th><th>Description</th></tr>"
-for project in content['items']:
-    html_content += f"<tr><td>{project['name']}</td><td><a href='{project['html_url']}'>{project['html_url']}</a></td><td>{project['description']}</td></tr>"
-html_content += "</table></body></html>"
+    response = requests.get(
+        'https://api.github.com/search/repositories', params=params)
+    content = json.loads(response.content)
 
+    html_content = "<table><tr><th>Name</th><th>HTML URL</th><th>Description</th></tr>"
+    for project in content['items']:
+        html_content += f"<tr><td>{project['name']}</td><td><a href='{project['html_url']}'>{project['html_url']}</a></td><td>{project['description']}</td></tr>"
+    html_content += "</table>"
+    return html_content
+
+
+def get_free_quota():
+    URL = os.environ.get("FREE_QUOTA")
+    response = requests.get(URL)
+    content = json.loads(response.content)
+    dt_now = dt.datetime.now()
+    remain_days = -1
+    if dt_now.day < content['bw_reset_day_of_month']:
+        remain_days = content['bw_reset_day_of_month'] - dt_now.day
+    else:
+        if dt_now.month == 12:
+            remain_days = (dt.datetime(year=dt_now.year+1, month=1,
+                           day=content['bw_reset_day_of_month']) - dt_now).days
+        else:
+            remain_days = (dt.datetime(year=dt_now.year, month=dt_now.month+1,
+                           day=content['bw_reset_day_of_month']) - dt_now).days
+    html_content = "<table><tr><th>Limit Usage</th><th>Used</th><th>ResetDay</th><th>RemainDays</th></tr>"
+    html_content += f"<tr><td>{content['monthly_bw_limit_b']/1000000000}G</td><td>{content['bw_counter_b']//1000000000}G</td><td>{content['bw_reset_day_of_month']}</td><td>{remain_days}</td></tr>"
+    html_content += "</table>"
+    return html_content
+
+
+html_content = "<html><body>" + get_github_repo_html() + get_free_quota() + \
+    "</body></html>"
 print(html_content)
