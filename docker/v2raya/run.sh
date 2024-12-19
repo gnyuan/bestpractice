0cdccf2adb16d6453fa0cb39e9b4ecb8bb7028fa
gnyuan <gnyuan@gmail.com>
2024-12-19 23:00:05 +0800

    fix

--- docker/v2raya/run.sh
+++ docker/v2raya/run.sh
@@ -8,7 +8,7 @@ docker run -d \
 -p 20171:20171 \
 -p 20172:20172 \
 -v /home/yuangn/v2raya/modules:/lib/modules \
--v /home/yuangn/v2raya/resolv.conf:/etc/resolv.conf \
+-v /home/yuangn/v2raya/resolv.conf:/etc/resolv.conf:ro \
 -v /home/yuangn/v2raya/v2raya:/etc/v2raya \
 -v /home/yuangn/v2raya/v2ray:/etc/v2ray \
 mzz2017/v2raya
